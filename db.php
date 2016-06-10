d9eec1acfa693ebe63808feb9372f521c30346f2
pranavgowtham <pranav.gowtham001@gmail.com>
2016-06-10 11:42:37 +0530

    changes made and added file

---
+++ db.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<body> 
+
+<?php
+$servername = "localhost";
+$Rollnumber = "Rollnumber";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $Rollnumber, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+$conn->close();
+?>
+</body>
+</html>
